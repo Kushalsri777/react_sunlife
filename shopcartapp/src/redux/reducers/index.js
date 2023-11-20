b87782dc6385a9134abbf585d77806a4b7a00b66
Kushalsri777 <69420616+Kushalsri777@users.noreply.github.com>
2023-11-19 22:07:23 -0800

    Delete shopcartapp directory

--- shopcartapp/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import { selectedProductReducer, productReducer} from './productReducer'
-
-const rootReducer = combineReducers({
-    allProducts:productReducer,
-    product:selectedProductReducer
-})
-
-export default rootReducer
